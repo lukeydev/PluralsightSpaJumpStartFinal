b49ca410cc6b8f02383e66d9fdf56136ddab776a
lukeydev <lukeypc@gmail.com>
2015-02-21 09:49:41 +1030

    ko training

---
+++ GroupManager/Scripts/Project/home.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../knockout-3.2.0.debug.js" />
+
+$(function () {
+    //alert('hello world');
+
+    var viewModel = {
+        firstName: ko.observable("John")
+    };
+
+    ko.applyBindings(viewModel);
+})
+
