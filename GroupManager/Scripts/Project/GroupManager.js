1850dbd206071a5a27f8582b0fb3988efce707ce
lukeydev <lukeypc@gmail.com>
2015-02-21 12:41:18 +1030

    group manager added

---
+++ GroupManager/Scripts/Project/GroupManager.js
@@ -0,0 +1,3 @@
+﻿/// <reference path="../knockout-3.2.0.debug.js" />
+
+
---
+++ GroupManager/Scripts/Project/data.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../knockout-3.2.0.debug.js" />
+var my = {}
+
+my.sampleData = (function (my) {
+    "use strict";
+    var data = {
+        Groups: [
+            { "GroupId": 1, "GroupName": "DOCS_SUPERVISORS", "Disable": "true" },
+            { "GroupId": 2, "GroupName": "DOCS_USERS", "Disable": "false" },
+            { "GroupId": 3, "GroupName": "RECORDS_SUPERVISORS", "Disable": "true" },
+            { "GroupId": 4, "GroupName": "DOCS_SUPERVISORS", "Disable": "true" },
+        ]
+    };
+    return {
+        data: data
+    };
+})(my);
