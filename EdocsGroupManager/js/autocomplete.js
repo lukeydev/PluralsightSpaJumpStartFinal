a42fc4c8c0c67cca587ce7db0c54163e9bb5ab0e
lukeydev <lukeypc@gmail.com>
2015-02-22 09:48:24 +1030

    New Project

---
+++ EdocsGroupManager/js/autocomplete.js
@@ -0,0 +1,62 @@
+﻿/// <reference path="../Scripts/jquery-2.1.3.min.js" />
+ko.bindingHandlers.autoComplete = {
+    // Only using init event because the Jquery.UI.AutoComplete widget will take care of the update callbacks
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        // { selected: mySelectedOptionObservable, options: myArrayOfLabelValuePairs }
+        var settings = valueAccessor();
+
+        var selectedOption = settings.selected;
+        var options = settings.options;
+
+        var updateElementValueWithLabel = function (event, ui) {
+            // Stop the default behavior
+            event.preventDefault();
+
+            // Update the value of the html element with the label 
+            // of the activated option in the list (ui.item)
+            if (ui.item != null) {
+                $(element).val(ui.item.label);
+
+                // Update our SelectedOption observable
+                if (typeof ui.item !== "undefined") {
+                    // ui.item - label|value|...
+                    selectedOption(ui.item);
+                }
+            }
+        };
+
+        $(element).autocomplete({
+            source: options,
+            select: function (event, ui) {
+                updateElementValueWithLabel(event, ui);
+            },
+            focus: function (event, ui) {
+                updateElementValueWithLabel(event, ui);
+            },
+            change: function (event, ui) {
+                updateElementValueWithLabel(event, ui);
+            }
+        });
+    }
+};
+
+// Array with original data
+//var remoteData = [{
+//    name: 'DOCS_SUPERVISORS',
+//    id: 1
+//}, {
+//    name: 'DM_USERS',
+//    id: 2
+//}, {
+//    name: 'RECORDS_MANAGERS',
+//    id: 3
+//}, {
+//    name: 'CSD_CORP_REC',
+//    id: 4
+//}, {
+//    name: 'CSD_ICTS_BUS_SUP',
+//    id: 5
+//}, {
+//    name: 'HR',
+//    id: 6
+//}];
---
+++ EdocsGroupManager/js/data.js
@@ -0,0 +1,61 @@
+﻿/// <reference path="../Scripts/knockout-3.2.0.debug.js" />
+var my = {};
+
+//add service pulls here, currently set to sample data..
+//currentlySet
+my.groupData = (function (my) {
+    "use strict";
+    var data = {
+        Groups: [
+            {"SystemId": 1, "GroupId": "DOCS_SUPERVISORS", "GroupName":"DM Supervisors"},  
+            {"SystemId": 2, "GroupId": "DOCS_USERS", "GroupName": "DM Users" },
+            {"SystemId": 3, "GroupId": "RECORDS_MANAGERS", "GroupName": "Records Managers" },
+            {"SystemId": 4, "GroupId": "PROJECTS", "GroupName": "Projects" }
+        ]
+    };
+    return {
+        data: data
+    };
+})(my);
+
+//available Groups
+my.availableGroupData = (function (my) {
+    "use strict";
+    var data = {
+        Groups: [
+            { "SystemId": 5, "GroupId": "BULLS", "GroupName": "Chicago Bulls" },
+            { "SystemId": 6, "GroupId": "HORNETS", "GroupName": "Charlotte Hornets" },
+            { "SystemId": 7, "GroupId": "HEAT", "GroupName": "Miami Heat" },
+            { "SystemId": 8, "GroupId": "SPURA", "GroupName": "San Antonio Spurs" }
+        ]
+    };
+    return {
+        data: data
+    };
+
+})(my);
+
+my.remoteData = (function (my) {
+    "use strict";
+    var data = {
+        Users: [
+            { "name": 'DOCS_SUPERVISORS', "id": 1 },
+            { "name": 'DM_USERS', "id": 2 },
+            { "name": 'RECORDS_MANAGERS', "id": 3 },
+            { "name": 'CSD_CORP_REC', "id": 4 },
+            { "name": 'CSD_ICTS_BUS_SUP', "id": 5 },
+            { "name": 'HR', "id": 6 }
+        ]
+    };
+    return {
+        data: data
+    };
+
+})(my);
+
+
+
+
+
+
+
